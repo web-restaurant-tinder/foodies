19c1dae28b04ca83d9678d719fde06e90f0e23d7
Francisco Gallegos <fgallegos59@cnm.edu>
2020-04-08 12:44:42 -0600

    added html

---
+++ epic/persona-by-francisco.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Persona</title>
+</head>
+<body>
+    <h1>Restaurant Tinder Persona</h1>
+        <ul>
+            <li>Name: Average Joe & Jane Doe</li>
+            <li>Age: 29 & 25</li>
+            <li>Gender: Male & Female</li>
+            <li>Occupation: Architect & Project Manager</li>
+            <li>Technology: Laptop, smartphone, tablet </li>
+        </ul>
+    <h2>Needs/Wants</h2>
+        <ul>
+            <li>Would like help in choosing a place to eat on a consistent basis.</li>
+            <li>Would like to be able to see what restaurants other people are  choosing.</li>
+            <li>Want to be able to connect with other users.</li>
+        </ul>
+    <h2>Frustrations</h2>
+        <ul>
+            <li>Can never both agree upon a restaurant.</li>
+            <li>Don't know what places are good choices.</li>
+        </ul>
+
+</body>
+</html>
