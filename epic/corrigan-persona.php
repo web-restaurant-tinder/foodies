ef0387d55ffe67465a41ae24e10d94ac38fa301e
Corrigan Newsome <cnewsome2@cnm.edu>
2020-04-10 09:06:50 -0600

    added persona file

---
+++ epic/corrigan-persona.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Corrigan Persona</title>
+</head>
+<body>
+<h1>Corrigan's Persona</h1>
+<img src="jeremy-pic.jpg" id="jeremy's-pic">
+
+<ul>
+	<li>Name: Jeremy</li>
+	<li>Age:24</li>
+	<li>Technology: Jeremy is young and very tech savvy. He uses all Apple products. He has an iPhone, iPad, Apple Watch, and a Mac Computer. All of these technologies are always using the latest software updates. </li>
+	<li>Profession: Working as a Graduate Assistant at a local University where he is working on his Master's in Philosophy and teaching philosophy to undergrads. </li>
+	<li>Jeremy uses his computer, phone, and tablet everyday. He will search the web to find fun things to do on the weekend as well as pull up Reddit to find answers to many of his questions. He will also watch YouTube videos to learn how to do new things.</li>
+</ul>
+</body>
+</html>
