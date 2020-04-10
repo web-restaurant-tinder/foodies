94c36d13e779f102e78c953a13f538a65d3ac8c8
Francisco Gallegos <fgallegos59@cnm.edu>
2020-04-10 09:49:10 -0600

    added use case interaction flow

--- epic/persona-by-francisco.php
+++ epic/persona-by-francisco.php
@@ -14,7 +14,7 @@
             <li>Age: 29 & 25</li>
             <li>Gender: Male & Female</li>
             <li>Occupation: Architect & Project Manager</li>
-            <li>Technology: Laptop, smartphone, tablet </li>
+            <li>Technology: Laptop, smartphone, tablet</li>
         </ul>
     </div>
     <h2>Needs/Wants</h2>
@@ -28,6 +28,21 @@
             <li>Can never both agree upon a restaurant.</li>
             <li>Don't know what places are good choices.</li>
         </ul>
+    <h2>Use Case: </h2>
+    <p><b>Title:</b>Finding a restaurant.</p>
+    <p><b>Description:</b>Joe and Jane want to find a place to eat that they both agree on.</p>
+    <p><b>Name of User and Their Role:</b>Average Joe and Jane Doe; foodies that enjoy trying new things.</p>
+    <p><b>Usage pre-conditions:</b>Must have a user account, a device to access application, and internet access.</p>
+    <p><b>Usage post-conditions:</b>A restaurant is chosen and is either liked or disliked.</p>
+    <h2>Interaction Flow:</h2>
+    <ul>
+        <li>First Jane & Joe select the app on their device</li>
+        <li>Then they add each other to a group</li>
+        <li>They begin swiping on a variety of different restaurants</li>
+        <li>The results are tallied</li>
+        <li>A mutual interest is selected</li>
+    </ul>
+    <p><b>Frequency of Use:</b> Friday nights and weekends. </p>
 
 </body>
 </html>
