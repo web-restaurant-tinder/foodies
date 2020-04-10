f48e8fbe19da88d99d05b702beb2e686db8e8d04
srendon <srendon4@cnm.edu>
2020-04-10 10:28:28 -0600

    Created a persona.php

---
+++ persona.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Corrigan Persona</title>
+</head>
+<body>
+<h1>Corrigan's Persona</h1>
+<div><img src="Electrician Jacob.jpg" height="390" width="600" alt="Makenzie Smith"/></div>
+
+<ul>
+	<li>Name: Jacob</li>
+	<li>Age:28</li>
+	<li>Technology: iPhone, Mac Pro, IPad. </li>
+	<li>Profession: Jacob is an electrician he installs, operates, maintains, and repairs electric devices or electrical wiring. </li>
+	<li>Jacob likes to go out on the weekend and usually searches on the web fun things to do and for that he usually uses his phone.</li>
+</ul>
+</body>
+</html>
