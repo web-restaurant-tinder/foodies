9595acb26d718af31feba7a9768939492eba2d15
srendon <srendon4@cnm.edu>
2020-04-10 10:43:59 -0600

    Created a persona.php

--- persona.php
+++ persona.php
@@ -11,9 +11,18 @@
 <ul>
 	<li>Name: Jacob</li>
 	<li>Age:28</li>
+	<li>Gender: Male</li>
 	<li>Technology: iPhone, Mac Pro, IPad. </li>
 	<li>Profession: Jacob is an electrician he installs, operates, maintains, and repairs electric devices or electrical wiring. </li>
 	<li>Jacob likes to go out on the weekend and usually searches on the web fun things to do and for that he usually uses his phone.</li>
 </ul>
+<h2>Frustrations</h2>
+<ul>
+	<li>Never really goes out and when he does he doesnt know where to eat.</li>
+	<li>Can never agree with his date on where to go.</li>
+</ul>
+<h2>Use Case: </h2>
+<p><b>Title:</b>Trying to find a restaurant with good reviews.</p>
+<p><b>Description:</b>Jacob wants to take his date out and finally agree on a place.</p>
 </body>
 </html>
