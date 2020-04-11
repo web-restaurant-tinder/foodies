fcad56bb7f934271c51dd69793081d2c6d628af2
Corrigan Newsome <cnewsome2@cnm.edu>
2020-04-11 12:38:50 -0600

    Added a few lines items of info.

--- epic/corrigan-persona.php
+++ epic/corrigan-persona.php
@@ -6,14 +6,29 @@
 </head>
 <body>
 <h1>Corrigan's Persona</h1>
-<img src="jeremy-pic.jpg" id="jeremy's-pic">
+	<img src="jeremy-pic.jpg" id="jeremy's-pic">
 
-<ul>
-	<li>Name: Jeremy</li>
-	<li>Age:24</li>
-	<li>Technology: Jeremy is young and very tech savvy. He uses all Apple products. He has an iPhone, iPad, Apple Watch, and a Mac Computer. All of these technologies are always using the latest software updates. </li>
-	<li>Profession: Working as a Graduate Assistant at a local University where he is working on his Master's in Philosophy and teaching philosophy to undergrads. </li>
-	<li>Jeremy uses his computer, phone, and tablet everyday. He will search the web to find fun things to do on the weekend as well as pull up Reddit to find answers to many of his questions. He will also watch YouTube videos to learn how to do new things.</li>
-</ul>
+	<ul>
+		<li>Name: Jeremy</li>
+		<li>Age:24</li>
+		<li>Technology: Jeremy is young and very tech savvy. He uses all Apple products. He has an iPhone, iPad, Apple Watch, and a Mac Computer. All of these technologies are always using the latest software updates. </li>
+		<li>Profession: Working as a Graduate Assistant at a local University where he is working on his Master's in Philosophy and teaching philosophy to undergrads. </li>
+		<li>Jeremy uses his computer, phone, and tablet everyday. He will search the web to find fun things to do on the weekend as well as pull up Reddit to find answers to many of his questions. He will also watch YouTube videos to learn how to do new things.</li>
+		<li>Jeremy has frustrations in not being able to quickly decide where he and a friend will go out to eat, he also has a difficult time on finding new restaurants that he would like to try in his area. </li>
+	</ul>
+
+<h3>User Story</h3>
+	<ul></ul>
+		<li></li>
+
+
+<h3>Use Case</h3>
+	<ul></ul>
+		<li></li>
+
+o90[
+<h3>Interaction Flow</h3>
+	<ul></ul>
+		<li></li>
 </body>
 </html>
