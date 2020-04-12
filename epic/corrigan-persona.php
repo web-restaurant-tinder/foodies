fbb17d33226f51d996b50565f7affb2565969d46
Corrigan Newsome <cnewsome2@cnm.edu>
2020-04-12 17:27:15 -0600

    added User Story, Use Case, and Interaction flow to the Persona.

--- epic/corrigan-persona.php
+++ epic/corrigan-persona.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <h1>Corrigan's Persona</h1>
-	<img src="jeremy-pic.jpg" id="jeremy's-pic">
+	<img src="jeremy-pic.jpg" id="jeremy's-pic" alt = "persona-pic">
 
 	<ul>
 		<li>Name: Jeremy</li>
@@ -18,17 +18,27 @@
 	</ul>
 
 <h3>User Story</h3>
-	<ul></ul>
-		<li></li>
-
+	<p>As a monthly user of Restaurant Tinder, Jeremy wants to be able to quickly and easily find somewhere to eat that he and friends will enjoy.</p>
 
 <h3>Use Case</h3>
-	<ul></ul>
-		<li></li>
+	<p>It is 7pm. Jeremy and his friend has listed dozens of possible places to go out to eat together and they have yet to come to an agreement. Jeremy remembers Restaurant Tinder. He then grabs his iPhone 10X and goes to the Restaurant Tinder app using the Safari browser.
+	He logs in and so does his friend. They begin swiping left or right to indicate a restaurant they like or do not like. After a few minutes of both of them swiping they realize they have matched on a number of restaurants. They finally pick a new restaurant to try out that they
+	both like.</p>
 
-o90[
 <h3>Interaction Flow</h3>
-	<ul></ul>
-		<li></li>
+	<ul>
+		<li>Jeremy pulls out his iPhone 10X which is using the latest iOS software update and navigates to the Safari browser. He then types in the Restaurant Tinder web address in the search bar.</li>
+		<li>The browser then opens up a set of results for the search.</li>
+		<li>Jeremy will then click the Restaurant Tinder link, which will bring him to the homepage of Restaurant Tinder.</li>
+		<li>Jeremy will then be brought to a login screen, Jeremy will put in his Username and Password and click the submit button.</li>
+		<li>A new screen will appear and it will be a screen that has a restaurant and two buttons on the bottom, Jeremy can click one of the two buttons on the bottom to indicate if he likes that restaurant or not.</li>
+		<li>Jeremy will then begin clicking on the like button or the no thanks button depending on his preferences.</li>
+		<li>After a few minutes of swiping, Jeremy will click an icon on top left of his current page.</li>
+		<li>A new screen will appear that lists the restaurant names that Jeremy indicated that he liked.</li>
+		<li>Meanwhile his friend would be doing the exact same thing.</li>
+		<li>They would compare lists and find a restaurant that they both like, and go to the restaurant that they mututally like.</li>
+		<li>Jeremy would then log out of the website and go to the restaurant with his friend.</li>
+	</ul>
+
 </body>
 </html>
