ffd8d854fcdffc63b61b10fcde4cd6ae58f61189
srendon <srendon4@cnm.edu>
2020-04-20 16:26:34 -0600

    persona

---
+++ epic/persona_Sara.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>persona_Sara</title>
+</head>
+<body>
+<h1>Persona</h1>
+<img src="Electrician Jacob.jpg" height="192" width="263" alt="Makenzie Smith"/>
+
+<ul>
+	<li>Name: Jacob</li>
+	<li>Age:28</li>
+	<li>Gender: Male</li>
+	<li>Technology: iPhone, Mac Pro, IPad. </li>
+	<li>Profession: Jacob is an electrician he installs, operates, maintains, and repairs electric devices or electrical wiring. </li>
+	<li>Jacob likes to go out on the weekend and usually searches on the web fun things to do and for that he usually uses his phone.</li>
+</ul>
+<h2>Frustrations</h2>
+<ul>
+	<li>Never really goes out and when he does he doesnt know where to eat.</li>
+	<li>Can never agree with his date on where to go.</li>
+</ul>
+<h2>Use Case: </h2>
+<p><b>Title:</b>Trying to find a restaurant with good reviews.</p>
+<p><b>Description:</b>Jacob wants to take his date out and finally agree on a place.</p>
+<p><b>Name of User:</b>Jacob.</p>
+<h2>Interaction Flow:</h2>
+<ul>
+	<li>User opens webpage on his Iphone.</li>
+	<li>User searches for the Tinder restaurant webpage.</li>
+	<li>User opens the webpage.</li>
+	<li>User signs up with his email, username, phone number, and password.</li>
+	<li>User uploads picture.</li>
+	<li>User goes to his email and confirms account.</li>
+	<li>User signs in.</li>
+	<li>User sees a picture of a restaurant with an caption to swipe left or right.</li>
+	<li>User starts swiping left on a restaurant he doesnt care for.</li>
+	<li>The webpage stores all the dislikes to not show again.</li>
+	<li>User swipes right on the second restaurant that he liked.</li>
+	<li>The webpage stores the liked restaurants and shows more like it</li>
+	<li>User searches for friends with the search bar.</li>
+	<li>The app brings up many pages.</li>
+	<li>User clicks follow to follow friends.</li>
+	<li>User gets a notification telling him he has a new followers.</li>
+	<li>User checks out the page and notices its his friends accepting his invite.</li>
+	<li>User sees his matches with his friends.</li>
+	<li>User logs out.</li>
+</ul>
+</body>
+</html>
