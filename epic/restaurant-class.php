5967ad1be89af87c43f0ef2f6694cec856773c99
Corrigan Newsome <cnewsome2@cnm.edu>
2020-04-23 11:04:12 -0600

    added getters and setters, need to read documentation to see if what try/catches I should add

--- epic/restaurant-class.php
+++ epic/restaurant-class.php
@@ -12,7 +12,7 @@ use Ramsey\Uuid\Uuid;
  * @package CNewsome2\Foodies
  */
 
-class Restaurants {
+class Restaurant {
 	use ValidateUuid;
 	/**
 	 * id for this Restaurant; this is the Primary Key
@@ -40,13 +40,13 @@ class Restaurants {
 
 	/**
 	 * Latitude of this Restaurant
-	 * @var integer $restaurantLat
+	 * @var float $restaurantLat
 	 */
 	private $restaurantLat;
 
 	/**
 	 * Longitude of this Restaurant
-	 * @var integer $restaurantLng
+	 * @var float $restaurantLng
 	 */
 	private $restaurantLng;
 
@@ -58,13 +58,13 @@ class Restaurants {
 
 	/**
 	 * Phone number for this Restaurant
-	 * @var integer $restaurantPhone
+	 * @var string $restaurantPhone
 	 */
 	private $restaurantPhone;
 
 	/**
 	 * Star Rating for this Restaurant
-	 * @var integer $restaurantStarRating
+	 * @var float $restaurantStarRating
 	 */
 	private $restaurantStarRating;
 
@@ -80,19 +80,21 @@ class Restaurants {
 	 * @param string $newRestaurantAddress
 	 * @param string $newRestaurantAvatar
 	 * @param string $newRestaurantFoodType
-	 * @param integer $newRestaurantLat
-	 * @param integer $newRestaurantLng
-	 * @param string $newrestaurantName
-	 * @param integer $newRestaurantPhone
-	 * @param integer $newRestaurantStarRating
+	 * @param float $newRestaurantLat
+	 * @param float $newRestaurantLng
+	 * @param string $newRestaurantName
+	 * @param string $newRestaurantPhone
+	 * @param float $newRestaurantStarRating
 	 * @param string $newRestaurantUrl
 	 * @throws \InvalidArgumentException if data types are not valid
 	 * @throws \RangeException if data values are out of bounds
 	 * @throws \TypeError if data types violate type hints
 	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
 	 */
 
-	public function __construct($newRestaurantId, string $newRestaurantAddress, string $newRestaurantAvatar, string $newRestaurantFoodType, integer $newRestaurantLat, integer $newRestaurantLng, string $newrestaurantName, integer $newRestaurantPhone, integer $newRestaurantStarRating, string $newRestaurantUrl) {
+	//add JONSERILIZABLE to constructor
+	public function __construct($newRestaurantId, string $newRestaurantAddress, string $newRestaurantAvatar, string $newRestaurantFoodType, float $newRestaurantLat, float $newRestaurantLng, string $newRestaurantName, string $newRestaurantPhone, float $newRestaurantStarRating, string $newRestaurantUrl) {
 		try {
 			$this->setRestaurantId($newRestaurantId);
 			$this->setRestaurantAddress($newRestaurantAddress);
@@ -100,7 +102,7 @@ class Restaurants {
 			$this->setRestaurantFoodType($newRestaurantFoodType);
 			$this->setRestaurantLat($newRestaurantLat);
 			$this->setRestaurantLng($newRestaurantLng);
-			$this->setRestaurantName($newrestaurantName);
+			$this->setRestaurantName($newRestaurantName);
 			$this->setRestaurantPhone($newRestaurantPhone);
 			$this->setRestaurantStarRating($newRestaurantStarRating);
 			$this->setRestaurantUrl($newRestaurantUrl);
@@ -113,8 +115,189 @@ class Restaurants {
 		}
 	}
 
+	/**
+	 * accessor method for restaurantId
+	 *
+	 * @return Uuid value of restaurantId
+	 */
+	public function getRestaurantId(): Uuid {
+		return ($this->restaurantId);
+	}
 
+	/**
+	 * mutator method for restaurantId
+	 *
+	 * @param Uuid|string $newRestaurantId new value of restaurantId
+	 * @throws \RangeException if $newRestaurantId is out of range
+	 * @throws \TypeError if $newRestaurantId is not a uuid or string
+	 */
+	public function setRestaurantId($newRestaurantId): void {
+		try {
+			$uuid = self::validateUuid($newRestaurantId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
 
+		$this->restaurantId = $uuid;
+	}
 
+	/**
+	 * accessor method for restaurantAddress
+	 *
+	 * @return string value of restaurantAddress
+	 */
+	public function getRestaurantAddress(): string {
+		return $this->restaurantAddress;
+	}
 
+	/**
+	 * @param string $restaurantAddress
+	 */
+	public function setRestaurantAddress(string $restaurantAddress): void {
+		$this->restaurantAddress = $restaurantAddress;
+	}
+
+	/**
+	 * accessor method for restaurantAvatar
+	 *
+	 * @return string
+	 */
+	public function getRestaurantAvatar(): string {
+		return $this->restaurantAvatar;
+	}
+
+	/**
+	 * mutator method for restaurantAvatar
+	 *
+	 * @param string $newRestaurantAvatar
+	 */
+	public function setRestaurantAvatar(?string $newRestaurantAvatar): void {
+		$this->restaurantAvatar = $newRestaurantAvatar;
+	}
+
+	/**
+	 * accessor method for restaurantFoodType
+	 *
+	 * @return string
+	 */
+	public function getRestaurantFoodType(): string {
+		return $this->restaurantFoodType;
+	}
+
+	/**
+	 * mutator method for restaurantFoodType
+	 * @param string $restaurantFoodType
+	 */
+	public function setRestaurantFoodType(string $restaurantFoodType): void {
+		$this->restaurantFoodType = $restaurantFoodType;
+	}
+
+	/**
+	 * accessor method for restaurantLat
+	 *
+	 * @return float
+	 */
+	public function getRestaurantLat(): float {
+		return $this->restaurantLat;
+	}
+
+	/**
+	 * mutator method for restaurantLat
+	 *
+	 * @param float $restaurantLat
+	 */
+	public function setRestaurantLat(float $restaurantLat): void {
+		$this->restaurantLat = $restaurantLat;
+	}
+
+	/**
+	 * accessor method for restaurantLng
+	 *
+	 * @return float
+	 */
+	public function getRestaurantLng(): float {
+		return $this->restaurantLng;
+	}
+
+	/**
+	 * mutator method for restaurantLng
+	 *
+	 * @param float $restaurantLng
+	 */
+	public function setRestaurantLng(float $restaurantLng): void {
+		$this->restaurantLng = $restaurantLng;
+	}
+
+	/**
+	 * accessor method for restaurantName
+	 *
+	 * @return string
+	 */
+	public function getRestaurantName(): string {
+		return $this->restaurantName;
+	}
+
+	/**
+	 * mutator method for restaurantName
+	 *
+	 * @param string $restaurantName
+	 */
+	public function setRestaurantName(string $restaurantName): void {
+		$this->restaurantName = $restaurantName;
+	}
+
+	/**
+	 * accessor method for restaurantPhone
+	 *
+	 * @return string
+	 */
+	public function getRestaurantPhone(): string {
+		return $this->restaurantPhone;
+	}
+
+	/**
+	 * mutator method for restaurantPhone
+	 *
+	 * @param string $restaurantPhone
+	 */
+	public function setRestaurantPhone(string $restaurantPhone): void {
+		$this->restaurantPhone = $restaurantPhone;
+	}
+
+	/**
+	 * accessor method for restaurantStarRating
+	 *
+	 * @return float
+	 */
+	public function getRestaurantStarRating(): float {
+		return $this->restaurantStarRating;
+	}
+
+	/**
+	 * mutator method for restaurantStarRating
+	 *
+	 * @param float $restaurantStarRating
+	 */
+	public function setRestaurantStarRating(float $restaurantStarRating): void {
+		$this->restaurantStarRating = $restaurantStarRating;
+	}
+
+	/**
+	 * accessor method for restaurantUrl
+	 *
+	 * @return string
+	 */
+	public function getRestaurantUrl(): string {
+		return $this->restaurantUrl;
+	}
+
+	/**
+	 * mutator method for restaurantUrl
+	 *
+	 * @param string $restaurantUrl
+	 */
+	public function setRestaurantUrl(string $restaurantUrl): void {
+		$this->restaurantUrl = $restaurantUrl;
+	}
 }
