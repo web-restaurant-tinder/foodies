61c6e097c6844ee5c2dc5d4361f85ad62351ca2d
srendon <srendon4@cnm.edu>
2020-04-26 11:18:53 -0600

    Created a classes folder for swipe.

--- epic/php/swipe.sql
+++ epic/php/swipe.sql
@@ -3,7 +3,7 @@ ALTER DATABASE srendon4 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 DROP TABLE IF EXISTS Swipe;
 
 CREATE TABLE Swipe(
-swipeProfileId BINARY(16) NOT NULL,
+swipeProfileId BINARY(128) NOT NULL,
 swipeRestaurantId CHAR(32),
 swipeDate VARCHAR (128),
 swipeRight VARCHAR (128) NOT NULL,
