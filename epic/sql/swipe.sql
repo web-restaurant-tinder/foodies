23f280865540d1786b30e005fdf337c28945ac25
srendon <srendon4@cnm.edu>
2020-04-27 12:03:46 -0600

    Created a classes folder for swipe.

---
+++ epic/sql/swipe.sql
@@ -0,0 +1,8 @@
+swipeProfileId BINARY(128) NOT NULL,
+swipeRestaurantId CHAR(32),
+swipeDate DATETIME(6),
+swipeRight BOOLEAN (128) NOT NULL,
+swipeLeft BOOLEAN(128) NOT NULL,
+FOREIGN KEY(swipeProfileId) REFERENCES swipe(swipeId),
+FOREIGN KEY(swipeRestaurantId) REFERENCES swipe(swipeId),
+);
