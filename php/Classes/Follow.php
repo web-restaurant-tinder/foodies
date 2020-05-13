6b1bdeb5fb78f112dbaa23acc69e9bfd94dca0db
Francisco Gallegos <fgallegos59@cnm.edu>
2020-05-13 15:09:45 -0600

    fixing more typos and adding missing content

---
+++ php/Classes/Follow.php
@@ -0,0 +1,362 @@
+<?php
+namespace WebRestaurantTinder\Foodies;
+
+require_once("autoload.php");
+require_once (dirname(__DIR__) . "/vendor/autoload.php");
+
+
+use Ramsey\Uuid\Uuid;
+
+/**
+ * This class was created to allow profiles to follow each other
+ * @author Francisco Gallegos <fgallegos59@cnm.edu>
+ */
+
+class Follow implements \JsonSerializable {
+    use ValidateDate;
+    use ValidateUuid;
+
+    /**
+     * Id for the followed profile; this is the foreign key
+     * @var Uuid $followFollowedProfileId
+     */
+
+    private $followFollowedProfileId;
+
+    /**
+     * Id for the profile following another profile; this is a foreign key
+     * @var Uuid $followProfileId
+     */
+
+    private $followProfileId;
+
+    /**
+     * Date that the profile was followed
+     *  @var \DateTime $newFollowDate
+     */
+
+    private $followDate;
+
+    /**
+     * constructor method for this class
+     *
+     * @param string|Uuid $newFollowFollowedProfileId id of this followed profile
+     * @param string|Uuid $newFollowProfileId id of the profile following other profile
+     * @param \DateTime|string|null $newFollowDate date and time follow occurs
+     */
+    public function __construct($newFollowFollowedProfileId, $newFollowProfileId, $newFollowDate = null)
+    {
+        try {
+            $this->setFollowFollowedProfileId($newFollowFollowedProfileId);
+            $this->setFollowProfileId($newFollowProfileId);
+            $this->setFollowDate($newFollowDate);
+        }
+        catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+            $exceptionType = get_class($exception);
+            throw(new $exceptionType($exception->getMessage(), 0, $exception));
+        }
+    }
+
+    /**
+     * accessor method for follow
+     * @return Uuid value of followed profile id
+     */
+    public function getFollowFollowedProfileId(): Uuid
+    {
+        return ($this->followFollowedProfileId);
+    }
+
+    /**
+     *mutator method for follow followed profile id
+     *
+     * @param mixed $newFollowFollowedProfileId
+     * @throws \RangeException if $newFollowFollowedProfileId is not positive
+     * @throws \TypeError if $newFollowFollowedProfileId is not an integer
+     */
+    public function setFollowFollowedProfileId($newFollowFollowedProfileId): void{
+        try {
+            $uuid = self::validateUuid($newFollowFollowedProfileId);
+        } catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+            $exceptionType = get_class($exception);
+            throw(new $exceptionType($exception->getMessage(), 0, $exception));
+        }
+
+        $this->followFollowedProfileId = $uuid;
+    }
+
+    /**
+     * accessor method for follow profile id
+     * @return Uuid value of follow profile id
+     */
+    public function getFollowProfileId(): Uuid
+    {
+        return $this->followProfileId;
+    }
+
+    /**
+     * mutator method for follow profile id
+     *
+     * @param string | Uuid $newFollowProfileId new value of follow profile id
+     * @throws \RangeException if $newFollowProfileId is not positive
+     * @throws \TypeError if $newFollowProfileId is not uuid or string
+     */
+    public function setFollowProfileId( $newFollowProfileId ) : void {
+        try {
+            $uuid = self::validateUuid($newFollowProfileId);
+        } catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+            $exceptionType = get_class($exception);
+            throw (new $exceptionType($exception->getMessage(), 0, $exception));
+        }
+
+        $this->followProfileId = $uuid;
+
+    }
+
+    /**
+     * accessor method for follow date
+     *
+     * @return \DateTime value of follow date
+     */
+    public function getFollowDate(): \DateTime
+    {
+        return $this->followDate;
+    }
+
+    /**
+     * mutator method for follow date
+     *
+     * @param \DateTime|string|null $newFollowDate follow date as a DateTime object or string
+     * @throws \InvalidArgumentException if $newFollowDate is not a valid object or string
+     * @throws \RangeException of $newFollowDate is date that does not exist
+     * @throws \Exception
+     */
+    public function setFollowDate($newFollowDate = null) : void {
+
+        if ($newFollowDate === null) {
+            $this->followDate = new  \DateTime();
+            return;
+        }
+
+        try {
+            $newFollowDate = self::validateDateTime($newFollowDate);
+        } catch (\InvalidArgumentException | \RangeException $exception) {
+            $exceptionType = get_class($exception);
+            throw(new $exceptionType($exception->getMessage(), 0, $exception));
+        }
+        $this->followDate= $newFollowDate;
+    }
+
+    /**
+     * inserts this Follown class into mySQL
+     *
+     * @param \PDO $pdo PDO connection object
+     * @return array
+     * @throws \PDOException when mySQL related errors occur
+     * @throws \TypeError if $pdo is not a PDO connection object
+     */
+    public function insert(\PDO $pdo) : void {
+        //create query template
+        $query = "INSERT INTO follow(followFollowedProfileId, followProfileId, followDate) VALUES(:followFollowedProfileId, :followProfileId, :followDate)";
+        $statement = $pdo->prepare($query);
+
+        //bind member variables to the place holders in the template
+        $formattedDate = $this->followDate->format("Y-m-d H:i:s.u");
+        $parameters = ["followProfileId" => $this->followProfileId->getBytes(), "followFollowedProfileId" => $this->followFollowedProfileId->getBytes(), "followDate" => $formattedDate];
+        $statement->execute($parameters);
+    }
+
+    /**
+     * deletes this Follow class for mySQL
+     *
+     * @param \PDO $pdo PDO connection object
+     * @return array
+     * @throws \PDOException when mySQL related errors occur
+     * @throws \TypeError if $pdo is not a PDO connection object
+     */
+    public function delete(\PDO $pdo) : void {
+        //create query template
+        $query = "DELETE FROM follow WHERE followProfileId = :followProfileId";
+        $statement = $pdo->prepare($query);
+
+        //bind the member variables to the place holder in the template
+        $parameters = ["followProfileId" => $this->followProfileId->getBytes(), "followFollowedProfileId" => $this->followFollowedProfileId->getBytes()];
+        $statement->execute($parameters);
+    }
+
+//    /**
+//     * @param \PDO $pdo PDO connection object
+//     * @throws \PDOException when mySQL related error occurs
+//     */
+//    public function update(\PDO $pdo) : void {
+//        //create query template
+//        $query = "UPDATE follow SET followFollowedProfileId = :followFollowedProfileId, followProfileId = :followProfileId, followDate = :followDate WHERE followProfileId = :followProfileId ";
+//        $statement = $pdo->prepare($query);
+//
+//        $formattedDate = $this->followDate->format("Y-m-d H:i:s.u");
+//        $parameters = ["followFollowedProfileId" => $this->followFollowedProfileId->getBytes(), "followProfileId" => $this->followProfileId->getBytes(), "followDate" => $formattedDate];
+//        $statement->execute($parameters);
+//
+//    }
+
+    public static function getFollowByFollowProfileId(\PDO $pdo, $followProfileId) : \SplFixedArray {
+        //sanitize the followProfileId before searching
+        try {
+            $followProfileId = self::validateUuid($followProfileId);
+        } catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+            throw(new \PDOException($exception->getMessage(),0, $exception));
+        }
+
+        //create query table
+        $query = "SELECT followProfileId, followFollowedProfileId, followDate FROM follow WHERE followProfileId = :followProfileId";
+        $statement = $pdo->prepare($query);
+
+        //bind follow id to the place holder in the template
+        $parameters = ["followProfileId" => $followProfileId->getBytes()];
+        $statement->execute($parameters);
+
+        //grab the follow id from mySQL
+        $follows = new \SplFixedArray($statement->rowCount());
+        $statement->setFetchMode(\PDO::FETCH_ASSOC);
+        while (($row = $statement->fetch()) !== false) {
+            try {
+                $follow = new Follow($row["followProfileId"], $row["followFollowedProfileId"], $row["followDate"]);
+                $follows[$follows->key()] = $follow;
+                $follows->next();
+            } catch (\Exception $exception) {
+                //if the row could not be converted, rethrow it
+                throw(new \PDOException($exception->getMessage(), 0, $exception));
+            }
+        }
+        return ($follows);
+    }
+
+    public static function getFollowByFollowProfileIdAndFollowFollowedProfileId(\PDO $pdo, string $followProfileId, string $followFollowedProfileId) : ?Follow {
+
+        try {
+            $followProfileId = self::validateUuid($followProfileId);
+        } catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+            throw(new \PDOException($exception->getMessage(), 0, $exception));
+        }
+
+        try {
+            $followFollowedProfileId = self::validateUuid($followFollowedProfileId);
+        } catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+            throw(new \PDOException($exception->getMessage(), 0, $exception));
+        }
+
+
+        $query = "SELECT followProfileId, followFollowedProfileId, followDate FROM follow WHERE followProfileId = :followProfileId AND followFollowedProfileId = :followFollowedProfileId";
+        $statement = $pdo->prepare($query);
+
+        $parameters = ["followProfileId" => $followProfileId->getBytes(), "followFollowedProfileId" => $followFollowedProfileId->getBytes()];
+        $statement->execute($parameters);
+
+        try {
+            $follow = null;
+            $statement->setFetchMode(\PDO::FETCH_ASSOC);
+            $row = $statement->fetch();
+            if($row !== false) {
+                $follow = new Follow($row["followProfileId"], $row["followFollowedProfileId"], $row["followDate"]);
+            }
+        } catch (\Exception $exception) {
+            throw(new \PDOException($exception->getMessage(), 0, $exception));
+        }
+        return ($follow);
+    }
+
+    /**
+     * gets followed count by followProfileId
+     *
+     * @param \PDO $pdo PDO connection object
+     * @param Uuid|string $followFollowedProfileId profile id to search for
+     * @return Follow|null Follow found or null if not found
+     * @throws \PDOException when mySQL related errors occur
+     * @throws \TypeError when a variable is not the correct data type
+     */
+    public static function getFollowCountByFollowFollowedProfileId(\PDO $pdo, $followFollowedProfileId) : \SplFixedArray {
+        //sanitize the followFollowedProfileId before searching
+
+        try {
+            $followFollowedProfileId = self::vaidateUuid($followFollowedProfileId);
+        } catch (\InvalidArgumentException | \RangeException | \TypeError $exception) {
+            throw (new \PDOException($exception->getMessage(), 0, $exception));
+        }
+
+        $query = "SELECT COUNT(*) FROM follow WHERE followFollowedProfileId = :followFollowedProfileId";
+        $statement = $pdo->prepare($query);
+        $parameters= ["followFollowedProfileId" => $followFollowedProfileId->getBytes()];
+        $count = $statement->rowCount($parameters);
+//        $follows = new \SplFixedArray($statement->rowCount());
+//        $statement->setFetchMode(\PDO::FETCH_ASSOC);
+//        while (($row = $statement->fetch()) !== false) {
+//            try {
+//                $follow = new Follow($row["followProfileId"], $row["followFollowedProfileId"], $row["followDate"]);
+//                $follows[$follows->key()] = $follow;
+//                $follows->next();
+//            } catch (\Exception $exception) {
+//                //if the row could not be converted, rethrow it
+//                throw(new \PDOException($exception->getMessage(), 0, $exception));
+//            }
+//        }
+        return($count);
+    }
+
+    /**
+     * gets follow count by followProfileId
+     *
+     * @param \PDO $pdo PDO connection object
+     * @param Uuid|string $followProfileId profile id to search for
+     * @return Follow|null Follow found or null if not found
+     * @throws \PDOException when mySQL related errors occur
+     * @throws \TypeError when a variable is not the correct data type
+     */
+    public static function getFollowCountByFollowProfileId(\PDO $pdo, $followProfileId) : ?Follow {
+        //sanitize the followProfileId before searching
+        try {
+            $followProfileId = self::vaidateUuid($followProfileId);
+        } catch (\InvalidArgumentException | \RangeException | \TypeError $exception) {
+            throw (new \PDOException($exception->getMessage(), 0, $exception));
+        }
+
+        $query = "SELECT followFollowedProfileId, followProfileId, followDate FROM follow WHERE followProfileId = :followProfileId";
+        $statement = $pdo->prepare($query);
+
+        $parameters = ["followProfileId" => $followProfileId->getBytes()];
+        $statement->execute($parameters);
+
+        try {
+            $follow = null;
+            $statement->setFetchMode(\PDO::FETCH_ASSOC);
+            $row = $statement->fetch();
+            if($row !== false) {
+                $follow = new Follow($row["followProfileId"], $row["followFollowedProfileId"], $row["followDate"]);
+            }
+        } catch (\Exception $exception) {
+
+            throw (new \PDOException($exception->getMessage(), 0, $exception));
+        }
+        return($follow);
+    }
+
+
+    /**
+     * formats the state variables for JSON serialization
+     *
+     * @return array resulting state variables to serialize
+     */
+    public function jsonSerialize() : array {
+        $fields = get_object_vars($this);
+
+        $fields["followFollowedProfileId"] = $this->followFollowedProfileId->toString();
+        $fields["followProfileId"] = $this->followProfileId->toString();
+
+        //format the date so that the front end can consume it
+        $fields["followDate"] = round(floatval($this->followDate->format("U.u")) * 1000);
+        return($fields);
+    }
+
+
+}
+
+
+
--- php/Classes/Test/FollowTest.php
+++ php/Classes/Test/FollowTest.php
@@ -2,8 +2,7 @@
 namespace WebRestaurantTinder\Foodies\Test;
 
 use WebRestaurantTinder\Foodies\Follow;
-
-require_once(dirname(__DIR__) . "/Test/DataDesignTest.php");
+use WebRestaurantTinder\Foodies\Profile;
 
 require_once(dirname(__DIR__) . "/autoload.php");
 
@@ -11,61 +10,81 @@ require_once (dirname(__DIR__, 2) . "/lib/uuid.php");
 
 class FollowTest extends DataDesignTest {
 
+    private $profile;
+    private $followedProfile;
     private $VALID_FOLLOW_FOLLOWED_PROFILE_ID = null;
     private $VALID_FOLLOW_PROFILE_ID = null;
     private $VALID_FOLLOW_DATE = null;
+    private $VALID_HASH;
+    private $VALID_ACTIVATION;
 
+
+    public final function setUp(): void {
+        parent::setUp();
+        // create a salt and hash for the mocked profile
+        $password = "abc123";
+        $this->VALID_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 8]);
+        $this->VALID_ACTIVATION = bin2hex(random_bytes(16));
+
+        $this->profile = new Profile(generateUuidV4(),  $this->VALID_ACTIVATION, "abhckb", "https://vsgdsd.com",
+        "gfhd@gmail.com", "Jim",  $this->VALID_HASH, "Johnson", "gsdsda" );
+        $this->profile->insert($this->getPDO());
+
+        $this->followedProfile = new Profile(generateUuidV4(),  $this->VALID_ACTIVATION, "kishsjb", "https://lutchj.com",
+            "ghdka@gmail.com", "Francisco",  $this->VALID_HASH, "Gallegos", "lasscsc" );
+        $this->followedProfile->insert($this->getPDO());
+
+        $this->VALID_FOLLOW_DATE = new \DateTime();
+    }
 
     public function testInsertValidFollow() : void {
         $numRows = $this->getConnection()->getRowCount("follow");
-
-        $followProfileId = generateUuidV4()->toString();
-        $follow = new Follow($followProfileId, $this->VALID_FOLLOW_FOLLOWED_PROFILE_ID, $this->VALID_FOLLOW_DATE);
+        var_dump($this->profile->getProfileId()->toString(), $this->followedProfile->getProfileId()->toString());
+        $follow = new Follow($this->profile->getProfileId()->toString(), $this->followedProfile->getProfileId()->toString(), $this->VALID_FOLLOW_DATE);
         $follow->insert($this->getPDO());
-
+        var_dump($follow);
         $numRowsAfterInsert = $this ->getConnection()->getRowCount("follow");
         self::assertEquals($numRows + 1, $numRowsAfterInsert);
 
-        $pdoFollow = Follow::getFollowByFollowProfileId($this->getPDO(), $follow->getFollowProfileId()->toString());
-        self::assertEquals($followProfileId, $pdoFollow->getFollowProfileId());
-        self::assertEquals($this->VALID_FOLLOW_FOLLOWED_PROFILE_ID, $pdoFollow->getFollowFollowedProfileId());
+        $pdoFollow = Follow::getFollowByFollowProfileIdAndFollowFollowedProfileId($this->getPDO(), $follow->getFollowProfileId()->toString());
+        self::assertEquals($this->profile->getProfileId(), $pdoFollow->getFollowProfileId());
+        self::assertEquals( $this->followedProfile->getProfileId(), $pdoFollow->getFollowFollowedProfileId());
         self::assertEquals($this->VALID_FOLLOW_DATE, $pdoFollow->getFollowDate());
 
 
     }
 
-    public function testUpdateValidFollow() : void {
-        $numRows = $this->getConnection()->getRowCount("follow");
-        $followProfileId = generateUuidV4()->getRowCount("follow");
-        $follow = new Follow($followProfileId, $this->VALID_FOLLOW_FOLLOWED_PROFILE_ID, $this->VALID_FOLLOW_DATE);
-        $follow->insert($this->getPDO());
-        $follow->update($this->getPDO());
-
-
-        $numRowsAfterInsert = $this->getConnection()->getRowCount("follow");
-        self::assertEquals($numRows + 1, $numRowsAfterInsert);
-
-        $pdoFollow = Follow::getFollowByFollowProfileId($this->getPDO(), $follow->getFollowProfileId()->toString());
-        self::assertEquals($followProfileId, $pdoFollow->getFollowProfileId());
-        self::assertEquals($this->VALID_FOLLOW_FOLLOWED_PROFILE_ID, $pdoFollow->getFollowFollowedProfileId());
-        self::assertEquals($this->VALID_FOLLOW_PROFILE_ID, $pdoFollow->getFollowProfileId());
-        self::assertEquals($this->VALID_FOLLOW_DATE, $pdoFollow->getFollowDate());
-
-    }
-
+//    public function testUpdateValidFollow() : void {
+//        $numRows = $this->getConnection()->getRowCount("follow");
+//        $followProfileId = generateUuidV4()->getRowCount("follow");
+//        $follow = new Follow($followProfileId, $this->VALID_FOLLOW_FOLLOWED_PROFILE_ID, $this->VALID_FOLLOW_DATE);
+//        $follow->insert($this->getPDO());
+//        $follow->update($this->getPDO());
+//
+//
+//        $numRowsAfterInsert = $this->getConnection()->getRowCount("follow");
+//        self::assertEquals($numRows + 1, $numRowsAfterInsert);
+//
+//        $pdoFollow = Follow::getFollowByFollowProfileId($this->getPDO(), $follow->getFollowProfileId()->toString());
+//        self::assertEquals($followProfileId, $pdoFollow->getFollowProfileId());
+//        self::assertEquals($this->VALID_FOLLOW_FOLLOWED_PROFILE_ID, $pdoFollow->getFollowFollowedProfileId());
+//        self::assertEquals($this->VALID_FOLLOW_PROFILE_ID, $pdoFollow->getFollowProfileId());
+//        self::assertEquals($this->VALID_FOLLOW_DATE, $pdoFollow->getFollowDate());
+//
+//    }
+//
 
     public function testDeleteValidFollow() : void {
         //count number of rows
         $numRows = $this->getConnection()->getRowCount("follow");
 
-        $followProfileId = generateUuidV4();
-        $follow = new Follow($followProfileId, $this->VALID_FOLLOW_FOLLOWED_PROFILE_ID, $this->VALID_FOLLOW_DATE);
+        $follow = new Follow($this->profile->getProfileId()->toString(), $this->followedProfile->getProfileId()->toString(), $this->VALID_FOLLOW_DATE);
         $follow->insert($this->getPDO());
 
 
         //delete the Follow from mySQl
-        $this->assertsEquals($numRows + 1, $this->getConnection()->getRowCount("follow"));
-        $follow->delete($this->getPDO);
+        $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("follow"));
+        $follow->delete($this->getPDO());
 
         $pdoFollow = Follow::getFollowByFollowProfileId($this->getPDO(), $follow->getFollowProfileId());
         $this->assertNull($pdoFollow);
@@ -76,67 +95,66 @@ class FollowTest extends DataDesignTest {
     public function testGetValidFollowByFollowProfileId() : void {
         $numRows = $this->getConnection()->getRowCount("follow");
 
-        $followProfileId = generateUuidV4()->toString();
-        $follow = new Follow($followProfileId, $this->VALID_FOLLOW_FOLLOWED_PROFILE_ID, $this->VALID_FOLLOW_DATE);
+        $follow = new Follow($this->profile->getProfileId()->toString(), $this->followedProfile->getProfileId()->toString(), $this->VALID_FOLLOW_DATE);
         $follow->insert($this->getPDO());
 
         $numRowsAfterInsert = $this->getConnection()->getRowCount("follow");
         self::assertEquals($numRows + 1, $numRowsAfterInsert);
 
         $pdoFollow = Follow::getFollowByFollowProfileId($this->getPDO(), $follow->getFollowProfileId()->toString());
-        self::assertEquals($pdoFollow->getFollowProfileId(), $followProfileId);
-        self::assertEquals($pdoFollow->getFollowFollowedProfileId(), $followProfileId);
+        self::assertEquals($this->profile->getProfileId(), $pdoFollow->getFollowProfileId());
+        self::assertEquals( $this->followedProfile->getProfileId(), $pdoFollow->getFollowFollowedProfileId());
         self::assertEquals($pdoFollow->getFollowDate(), $this->VALID_FOLLOW_DATE);
     }
 
     public function testGetValidFollowCountByFollowFollowedProfileId() : void {
         $numRows = $this->getConnection()->getRowCount("follow");
 
-        $followProfileId = generateUuidV4()->toString();
-        $follow = new Follow($followProfileId, $this->VALID_FOLLOW_FOLLOWED_PROFILE_ID, $this->VALID_FOLLOW_DATE);
+        $follow = new Follow($this->profile->getProfileId()->toString(), $this->followedProfile->getProfileId()->toString(), $this->VALID_FOLLOW_DATE);
         $follow->insert($this->getPDO());
 
         $numRowsAfterInsert = $this->getConnection()->getRowCount("follow");
         self::assertEquals($numRows + 1, $numRowsAfterInsert);
 
-        $pdoFollow = Follow::getFollowByFollowProfileId($this->getPDO(), $follow->getFollowProfileId()->toString());
-        self::assertEquals($pdoFollow->getFollowProfileId(), $followProfileId);
-        self::assertEquals($pdoFollow->getFollowFollowedProfileId(), $followProfileId);
-        self::assertEquals($pdoFollow->getFollowDate(), $this->VALID_FOLLOW_DATE);
+        $pdoFollow = Follow::getFollowCountByFollowFollowedProfileId($this->getPDO(), $follow->getFollowProfileId()->toString());
+        var_dump($pdoFollow);
+//        self::assertEquals($this->profile->getProfileId(), $pdoFollow->getFollowProfileId());
+//        self::assertEquals( $this->followedProfile->getProfileId(), $pdoFollow->getFollowFollowedProfileId());
+//        self::assertEquals($pdoFollow->getFollowDate(), $this->VALID_FOLLOW_DATE);
     }
 
     public function testGetValidFollowCountByFollowProfileId() : void {
         $numRows = $this->getConnection()->getRowCount("follow");
 
-        $followProfileId = generateUuidV4()->toString();
-        $follow = new Follow($followProfileId, $this->VALID_FOLLOW_FOLLOWED_PROFILE_ID, $this->VALID_FOLLOW_DATE);
+        $follow = new Follow($this->profile->getProfileId()->toString(), $this->followedProfile->getProfileId()->toString(), $this->VALID_FOLLOW_DATE);
         $follow->insert($this->getPDO());
 
         $numRowsAfterInsert = $this->getConnection()->getRowCount("follow");
         self::assertEquals($numRows + 1, $numRowsAfterInsert);
 
-        $pdoFollow = Follow::getFollowByFollowProfileId($this->getPDO(), $follow->getFollowProfileId()->toString());
-        self::assertEquals($pdoFollow->getFollowProfileId(), $followProfileId);
-        self::assertEquals($pdoFollow->getFollowFollowedProfileId(), $followProfileId);
-        self::assertEquals($pdoFollow->getFollowDate(), $this->VALID_FOLLOW_DATE);
+        $pdoFollow = Follow::getFollowCountByFollowProfileId($this->getPDO(), $follow->getFollowProfileId()->toString());
+        var_dump($pdoFollow);
+//        self::assertEquals($this->profile->getProfileId(), $pdoFollow->getFollowProfileId());
+//        self::assertEquals( $this->followedProfile->getProfileId(), $pdoFollow->getFollowFollowedProfileId());
+//        self::assertEquals($pdoFollow->getFollowDate(), $this->VALID_FOLLOW_DATE);
     }
 
-    public function testGetValidFollows() : void {
-        $numRows = $this->getConnection()->getRowCount("follow");
-        $rowsInserted = 5;
-
-        for ($i=0; $i<$rowsInserted; $i++){
-            $followProfileId = generateUuidV4()->toString();
-            $follow = newFollow($followProfileId, $this->VALID_FOLLOW_FOLLOWED_PROFILE_ID,
-                 $this->VALID_FOLLOW_PROFILE_ID,
-                 $this->VALID_FOLLOW_DATE);
-            $follow->insert($this->getPDO());
-        }
-
-        self::assertsEquals($numRows + $rowsInserted, $this->getConnection()->getRowCount("follow"));
-
-        self::assertsEquals($numRows + $rowsInserted, $follow->getAllFollows($this->getPDO())->count());
-    }
+//    public function testGetValidFollows() : void {
+//        $numRows = $this->getConnection()->getRowCount("follow");
+//        $rowsInserted = 5;
+//
+//        for ($i=0; $i<$rowsInserted; $i++){
+//            $followProfileId = generateUuidV4()->toString();
+//            $follow = newFollow($followProfileId, $this->VALID_FOLLOW_FOLLOWED_PROFILE_ID,
+//                 $this->VALID_FOLLOW_PROFILE_ID,
+//                 $this->VALID_FOLLOW_DATE);
+//            $follow->insert($this->getPDO());
+//        }
+//
+//        self::assertsEquals($numRows + $rowsInserted, $this->getConnection()->getRowCount("follow"));
+//
+//        self::assertsEquals($numRows + $rowsInserted, $follow->getAllFollows($this->getPDO())->count());
+//    }
 
 
 }
