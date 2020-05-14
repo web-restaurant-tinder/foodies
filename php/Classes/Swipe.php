5486110c479d739499a4edb8f0c008f40471f968
srendon <srendon4@cnm.edu>
2020-05-14 08:49:04 -0600

    worked on class

--- php/Classes/Swipe.php
+++ php/Classes/Swipe.php
@@ -5,15 +5,13 @@ require_once("autoload.php");
 require_once(dirname(__DIR__) . "/vendor/autoload.php");
 
 use Sararendon01\TinderRestaurant\ValidateDate;
-use Sararendon01\TinderRestaurant\ValidateUuid;
 use Ramsey\Uuid\Uuid;
 
 /**
  * This is the Swipe class
  * @author Sara Rendon <srendon4@cnm.edu>
  */
-class Swipe implements \JsonSerializable
-{
+class Swipe implements \JsonSerializable {
 
 	use ValidateUuid;
 	use ValidateDate;
@@ -23,14 +21,12 @@ class Swipe implements \JsonSerializable
 
 	private $swipeDate;
 
-	private  $swipeRight;
+	private $swipeRight;
 
 	private $swipeLeft;
 
 
-
-	public function __construct($newSwipeProfileId, string $newSwipeRestaurantId, string $newSwipeDate, string $newSwipeRight, string $newSwipeLeft)
-	{
+	public function __construct($newSwipeProfileId, string $newSwipeRestaurantId, string $newSwipeDate, string $newSwipeRight, string $newSwipeLeft) {
 		try {
 			$this->setSwipeProfileId($newSwipeProfileId);
 			$this->setSwipeRestaurantId($newSwipeRestaurantId);
@@ -38,7 +34,7 @@ class Swipe implements \JsonSerializable
 			$this->setSwipeRight($newSwipeRight);
 			$this->setSwipeLeft($newSwipeLeft);
 
-		} catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
@@ -49,8 +45,7 @@ class Swipe implements \JsonSerializable
 	 *
 	 * @return Uuid value of Swipe Profile Id
 	 */
-	public function getSwipeProfileId(): Uuid
-	{
+	public function getSwipeProfileId(): Uuid {
 		return ($this->SwipeProfileId);
 	}
 
@@ -62,11 +57,10 @@ class Swipe implements \JsonSerializable
 	 * @throws \RangeException if #newSwipeProfileId is out of range
 	 * @throws \TypeError if $newSwipeProfileId is not a uuid or string
 	 */
-	public function setSwipeProfileId($newSwipeProfileId): void
-	{
+	public function setSwipeProfileId($newSwipeProfileId): void {
 		try {
 			$uuid = self::validateUuid($newSwipeProfileId);
-		} catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
@@ -79,8 +73,7 @@ class Swipe implements \JsonSerializable
 	 *
 	 * @return string value of the Swipe Restaurant Id
 	 */
-	public function getSwipeRestaurantId(): ?string
-	{
+	public function getSwipeRestaurantId(): ?string {
 		return $this->swipeRestaurantId;
 	}
 
@@ -92,25 +85,24 @@ class Swipe implements \JsonSerializable
 	 * @throws \RangeException if the id is not exactly 32 characters
 	 * @throws \TypeError if the Swipe Restaurant Id is not a string
 	 */
-	public function setSwipeRestaurantId(?string $newSwipeRestaurantId): void
-	{
+	public function setSwipeRestaurantId(?string $newSwipeRestaurantId): void {
 
-		if ($newSwipeRestaurantId === null) {
+		if($newSwipeRestaurantId === null) {
 			$this->SwipeRestaurantId = null;
 			return;
 		}
 
 		try {
 			$newSwipeRestaurantId = strtolower(trim($newSwipeRestaurantId));
-			if (ctype_xdigit($newSwipeRestaurantId) === false) {
+			if(ctype_xdigit($newSwipeRestaurantId) === false) {
 				throw(new\RangeException("swipe restaurant is not valid"));
 			}
 
 			//make sure user Swipe Restaurant is only 32 characters
-			if (strlen($newSwipeRestaurantId) !== 32) {
+			if(strlen($newSwipeRestaurantId) !== 32) {
 				throw(new\RangeException("swipe restaurant token has to be 32 characters"));
 			}
-		} catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
@@ -123,8 +115,7 @@ class Swipe implements \JsonSerializable
 	 *
 	 * @return string this Swipe Date
 	 */
-	public function getSwipeDate(): string
-	{
+	public function getSwipeDate(): string {
 		return $this->SwipeDate;
 	}
 
@@ -136,18 +127,17 @@ class Swipe implements \JsonSerializable
 	 * @throws \RangeException if the Swipe Date is not more than 255 characters
 	 * @throws \TypeError if the Swipe Date is not a string
 	 */
-	public function setSwipeDate(?string $newSwipeDate): void
-	{
+	public function setSwipeDate(?string $newSwipeDate): void {
 
 		$newSwipeDate = trim($newSwipeDate);
 		$newSwipeDate = filter_var($newSwipeDate, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 		try {
 			// verify the Swipe Date will fit in the database
-			if (strlen($newSwipeDate) > 255) {
+			if(strlen($newSwipeDate) > 255) {
 				throw(new \RangeException("date too long, must be less than 256 characters"));
 			}
-		} catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
@@ -159,8 +149,7 @@ class Swipe implements \JsonSerializable
 	 *
 	 * @return string swipe right
 	 */
-	public function getSwipeRight(): string
-	{
+	public function getSwipeRight(): string {
 		return $this->SwipeRight;
 	}
 
@@ -172,36 +161,25 @@ class Swipe implements \JsonSerializable
 	 * @throws \RangeException if the swipe is more than 128 characters
 	 * @throws \TypeError if the swipe is not a string
 	 */
-	public function setSwipeRight(string $newSwipeRight): void
-	{
+	public function setSwipeRight(string $newSwipeRight): void {
 		try {
 			// verify the Swipe Right is secure
 			$newSwipeRight = trim($newSwipeRight);
 			$newSwipeRight = filter_var($newSwipeRight, FILTER_VALIDATE_EMAIL);
-			if (empty($newSwipeRight) === true) {
+			if(empty($newSwipeRight) === true) {
 				throw(new \InvalidArgumentException("swipe right is empty or insecure"));
 			}
 
 			// verify the swipe right will fit in the database
-			if (strlen($newSwipeRight) > 128) {
+			if(strlen($newSwipeRight) > 128) {
 				throw(new \RangeException("swipe right is too large"));
 			}
-		}catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception){
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
 
 		$this->SwipeRight = $newSwipeRight;
-	}
-
-	/**
-	 * accessor for swipe left
-	 *
-	 * @return string this swipe left
-	 */
-	public function getSwipeLeft(): string
-	{
-		return $this->swipeleft;
 	}
 
 	/**
@@ -212,35 +190,45 @@ class Swipe implements \JsonSerializable
 	 * @throws \RangeException if the swipe left is larger than 97 characters
 	 * @throws \TypeError if the swipe left is not a string
 	 */
-	public function setSwipeLeft(string $newSwipeLeft): void
-	{
+	public function setSwipeLeft(string $newSwipeLeft): void {
 		try {
 			//enforce that the Swipe Left is properly formatted
 			$newSwipeLeft = trim($newSwipeLeft);
-			if(empty($newSwipeLeft) === true)
-				}
-
-			// verify the Swipe Left will fit in the database
-			if (strlen($newSwipeLeft) > 97) {
-				throw(new \RangeException("Swipe Left is too large"));
+			if(empty($newSwipeLeft) === true) {
+				throw(new \InvalidArgumentException("swipe is empty"));
 			}
-		}catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception){
+
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
-
-		$this->SwipeLeft = $newSwipeLeft;
 	}
 
+	/**
+	 * @inheritDoc
+	 */
+	public function jsonSerialize() {
+		// TODO: Implement jsonSerialize() method.
+	}
+}
+
+
+/**
+ * accessor for swipe left
+ *
+ * @return string this swipe left
+ */
+public function getSwipeLeft(): string {
+	return $this->swipeleft;
+}
 /**
  * @inheritDoc
  */
-public function jsonSerialize()
-{
+public function jsonSerialize(){
 	$fields = get_object_vars($this);
-	$fields["authorId"] = $this->authorId->toString();
-	unset($fields["authorActivationToken"]);
-	unset($fields["authorHash"]);
-	return ($fields);
+	$fields["profileId"] = $this->profileId->toString();
+	unset($fields["profileActivationToken"]);
+	unset($fields["profileHash"]);
+	return($fields);
 }
 }
