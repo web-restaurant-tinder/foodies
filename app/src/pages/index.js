4e876885ee89fcd0f4a7b65533a817725d421577
srendon <srendon4@cnm.edu>
2020-05-20 14:32:06 -0600

    created static

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -4,10 +4,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
-import {Home} from "./pages/Home";
+import {Home} from "./Home";
+import {signup} from  "./pages/signup";
 
 const Routing = () => (
 	<>
+		<signup>
+
+		</signup>
 		<BrowserRouter>
 			<Switch>
 				<Route exact path="/" component={Home}/>
---
+++ app/src/pages/signup.js
@@ -0,0 +1,28 @@
+import React from "react"
+import "./style.css"
+export const SignUp = () => {
+	return (
+		<card id={"signUpCard"}>
+			<h3>Sign Up</h3>
+			<div className="form-group" id="emailAddressGroup">
+				<label>Email address</label>
+				<input type="email" className="form-control" placeholder="Enter email" />
+			</div>
+			<div className="form-group" id="passwordGroup">
+				<label>Password</label>
+				<input type="password" className="form-control" placeholder="Enter password" />
+			</div>
+			<div className="form-group" id="checkBoxGroup">
+				<div className="custom-control custom-checkbox">
+					<input type="checkbox" className="custom-control-input" id="customCheck1" />
+					<label className="custom-control-label" htmlFor="customCheck1">Remember Me</label>
+				</div>
+			</div>
+			<div className="buttonGroup">
+				<button type="submit" className="btn btn-primary btn-block">Submit</button>
+				<p className="forgot-password text-right" id="forgotPassword"> Forgot <a href="#">password?</a>
+				</p>
+			</div>
+		</card>
+	);
+}
