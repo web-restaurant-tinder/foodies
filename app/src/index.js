61f5608f2df7d89910a3c24214ad1b0f4e46982f
Nathan Ortiz <nkortiz92@gmail.com>
2020-05-20 18:50:59 -0600

    added modals to page

--- app/src/index.js
+++ app/src/index.js
@@ -4,19 +4,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
-import {Navigation} from "./shared/components/Navigation";
+import {MainNav} from "./shared/components/main-nav/MainNav";
 import {Home} from "./pages/Home";
 // import {Test2} from "./pages/Test2";
 import {Jumbo} from "./shared/components/Jumbo"
 import {Choices} from "./shared/components/Choices";
-import "./index.css"
 
 const Routing = () => (
 	<>
-		<Navigation/>
-		<Jumbo/>
-		<Choices/>
 		<BrowserRouter>
+			<MainNav/>
+			<Jumbo/>
+			<Choices/>
 			<Switch>
 				<Route exact path="/" component={Home}/>
 				{/*<Route exact path="/test2" component={Test2}/>*/}
---
+++ app/src/shared/components/main-nav/MainNav.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {LinkContainer} from "react-router-bootstrap"
+import {SignUpModal} from "./sign-up/SignUpModal";
+import {SignInModal} from "./sign-in/SigninModal";
+
+
+
+export const MainNav = (props) => {
+	return(
+		<Navbar bg="primary" variant="dark">
+			<LinkContainer exact to="/" >
+				<Navbar.Brand>Home</Navbar.Brand>
+			</LinkContainer>
+			<Nav className="mr-auto">
+				<LinkContainer exact to="/followers">
+					<Nav.Link>Followers</Nav.Link>
+				</LinkContainer>
+				<LinkContainer exact to="/likes"
+				><Nav.Link>Likes</Nav.Link>
+				</LinkContainer>
+				<SignUpModal/>
+				<SignInModal/>
+			</Nav>
+		</Navbar>
+	)
+};
