cf02a109757b02f9bf456c94b7f715075e62543c
Corrigan Newsome <cnewsome2@cnm.edu>
2020-05-25 23:21:32 -0400

    fixed follow-reducer and get-follow, need to get an instructor to help with this last bit

--- app/src/shared/actions/get-follow.js
+++ app/src/shared/actions/get-follow.js
@@ -1,11 +1,6 @@
-import {httpConfig} from "../utils/http-config";
-
-export const getFollow = () => async dispatch => {
-	const {data} = await httpConfig('/apis/users/');
-	dispatch({type: "GET_ALL_FOLLOWS", payload: data })
-};
+import {httpConfig} from "/app/src/shared/utils/http-config";
 
 export const getFollowByProfileId = (id) => async dispatch => {
 	const {data} = await httpConfig(`/apis/followers/${id}`);
-	dispatch({type: "GET_FOLLOW_BY_FOLLOW_ID", payload: data })
+	dispatch({type: "GET_FOLLOW_BY_PROFILE_ID", payload: data })
 };
--- app/src/shared/reducers/follow-reducer.js
+++ app/src/shared/reducers/follow-reducer.js
@@ -1,7 +1,5 @@
 export default (state = [], action) => {
 	switch(action.type) {
-		case "GET_ALL_FOLLOWS":
-			return action.payload;
 		case "GET_FOLLOW_BY_PROFILE_ID":
 			return [...state, action.payload];
 		default:
